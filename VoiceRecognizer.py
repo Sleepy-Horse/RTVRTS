f7f4fab2cddfaa71af6f4c88b76747bcecedf11c
Sleepy-Horse <97367938+Sleepy-Horse@users.noreply.github.com>
2023-02-24 10:46:45 +0100

    Add output_language and language_codes to config.py and update VoiceRecognizer.py
    This commit adds two new variables, output_language and language_codes, to the config.py file. The output_language variable is used to specify the desired output language for the application, while the language_codes variable is a dictionary that maps language names to their respective ISO codes.
    
    In addition, this commit includes minor changes to the VoiceRecognizer.py file to improve the handling of language settings.

--- VoiceRecognizer.py
+++ VoiceRecognizer.py
@@ -1,16 +1,15 @@
 import time
 import speech_recognition as sr
-
 import config
 
 # Initialize the SpeechRecognizer object
 r = sr.Recognizer()
 
 # Get the language configuration from the config file
-language = config.language
+language = config.language_codes[config.input_language]
 
 # Open a log file to record any errors encountered during speech recognition
-log_file = open(f"logs/{time.time()}")
+log_file = open(f"logs/{time.time()}", 'w')
 
 
 # Define a function to get spoken words from the user
@@ -21,10 +20,13 @@ def get_spoken_text():
 
     try:
         # Attempt to recognize the spoken words using Google Speech Recognition API
-        return r.recognize_google(audio, language=language)
+        text = r.recognize_google(audio, language=language)
+        log_file.write(f"{time.time()} - {text}\n")
+        return text
     except sr.UnknownValueError:
         # If the SpeechRecognizer was unable to recognize any spoken words, return an empty string
         return ""
     except sr.RequestError as e:
         # If there was an error with the Google Speech Recognition service, record the error in the log file
-        log_file.write("Could not request results from Google Speech Recognition service; {0}\n".format(e))
+        log_file.write(f"{time.time()} - Could not request results from Google Speech Recognition service; {e}\n")
+        return ""
